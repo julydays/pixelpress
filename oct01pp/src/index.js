31c81164674bc5a7adb57209b4d338f03de2da6e
Ramirez <ramirez@ramirezs-mbp.home>
2017-10-01 11:05:13 -0400

    removed App component

--- oct01pp/src/index.js
+++ oct01pp/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Home from './components/home/Home';
 import registerServiceWorker from './registerServiceWorker';
 
