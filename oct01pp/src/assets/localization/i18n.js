446da7fd92e06c5c2e6133ffdb44d298eacd81ec
Evan Armbrister <evan.armbrister@traveliko.com>
2017-10-22 22:29:07 +0700

    pushed localaization

---
+++ oct01pp/src/assets/localization/i18n.js
@@ -0,0 +1,43 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+
+i18n
+    .use(LanguageDetector)
+    .init({
+        // we init with resources
+        resources: {
+            en: {
+                translations: {
+                    "LANGUAGE": "I'm from Brooklyn, I speak English.",
+                    "HOME": "Home",
+                    "COPIES": "Copies"
+                }
+            },
+            es: {
+                translations: {
+                    "LANGUAGE": "Yo vivo en Brooklyn Tambien.",
+                    "HOME": "Inicio",
+                    "COPIES": "Ejemplares"
+                }
+            }
+        },
+        fallbackLng: 'en',
+
+        // have a common namespace used around the full app
+        ns: ['translations'],
+        defaultNS: 'translations',
+
+        keySeparator: false, // we use content as keys
+
+        interpolation: {
+            escapeValue: false, // not needed for react!!
+            formatSeparator: ','
+        },
+
+        react: {
+            wait: true
+        }
+    });
+
+export default i18n;
--- oct01pp/src/index.js
+++ oct01pp/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
-import LocalizedStrings, { Text }  from 'react-localization';
-import { strings } from './localization/localization';
-import './index.css';
 import Home from './components/home/Home';
 import Copies from './components/copies/Copies';
 import About from './components/about/About';
+import './index.css';
+
+// localization dependencies
+import { I18nextProvider } from 'react-i18next';
+import i18n from './assets/localization/i18n';
+
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -22,5 +25,6 @@ const Root = () => {
         </Router>
     )
 };
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+ReactDOM.render(<I18nextProvider i18n={ i18n }><Root /></I18nextProvider>, document.getElementById('root'));
 registerServiceWorker();
